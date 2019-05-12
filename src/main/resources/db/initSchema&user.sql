77682799b58e9e1b48e386b144b382f2ccf58a63
Kirilo Lozitsky <bondxxl01@gmail.com>
2019-05-12 17:00:07 +0300

    Added spring dependency for orm. Added Spring beans for integration with the hibernate session.

--- src/main/resources/db/initSchema&user.sql
+++ src/main/resources/db/initSchema&user.sql
@@ -1,11 +1,20 @@
 # https://losst.ru/sozdanie-polzovatelya-mysql
 
-CREATE SCHEMA if not exists `spring-library`;
+CREATE SCHEMA if not exists `spring_library`;
 
 CREATE USER if not exists 'admin'@'localhost' IDENTIFIED BY 'password';
 
 SELECT User,Host FROM mysql.user;
 
-GRANT ALL PRIVILEGES ON `spring-library` . * TO 'admin'@'localhost';
+GRANT ALL PRIVILEGES ON `spring_library` . * TO 'admin'@'localhost';
 
 SHOW GRANTS FOR 'admin'@'localhost';
+
+# D:\mysql\bin>mysqldump.exe -u admin -p spring-library > d:\database.db
+# D:\mysql\bin>mysql.exe -u admin -p spring_library < d:\database.db
+#
+# D:\mysql\bin>mysql -u admin -p
+# mysql> drop schema `spring-library`;
+# mysql> exit
+# D:\mysql\bin>del d:\database.db
+
