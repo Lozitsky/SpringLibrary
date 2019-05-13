6de1a2517de17be7db7e43a8c3cb548deefde195
Kirilo Lozitsky <bondxxl01@gmail.com>
2019-05-13 15:21:55 +0300

    Created component DAO for working with book table. Used hibernate for giving list of books. Changed name of schema.

---
+++ src/main/java/com/kirilo/training/springlibrary/impl/BookDAOImpl.java
@@ -0,0 +1,48 @@
+package com.kirilo.training.springlibrary.impl;
+
+import com.kirilo.training.springlibrary.interfaces.BookSearch;
+import com.kirilo.training.springlibrary.objects.Author;
+import com.kirilo.training.springlibrary.entities.Book;
+import com.kirilo.training.springlibrary.objects.Genre;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class BookDAOImpl implements BookSearch {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    private List<Book> books;
+
+    //    https://stackoverflow.com/questions/14423664/hibernate-get-list-from-database
+    @Transactional
+    @Override
+    public List<Book> getBooks() {
+        books = sessionFactory.getCurrentSession().createSQLQuery("SELECT * FROM book").addEntity(Book.class).list();
+        return books;
+    }
+
+    @Override
+    public List<Book> getBooks(Author author) {
+        return null;
+    }
+
+    @Override
+    public List<Book> getBooks(Genre genre) {
+        return null;
+    }
+
+    @Override
+    public List<Book> getBooks(String bookName) {
+        return null;
+    }
+
+    @Override
+    public List<Book> getBooks(Character letter) {
+        return null;
+    }
+}
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,9 +1,17 @@
-INSERT INTO `spring-library`.author (full_name, birthday) VALUES ('name lastname', '1999-01-01');
+INSERT INTO `spring_library`.author (full_name, birthday) VALUES ('name lastname1', '1999-01-01');
+INSERT INTO `spring_library`.author (full_name, birthday) VALUES ('name lastname2', '1999-01-01');
+INSERT INTO `spring_library`.author (full_name, birthday) VALUES ('name lastname3', '1999-01-01');
 
-INSERT INTO `spring-library`.genre (name) VALUES ('some genre2');
+INSERT INTO `spring_library`.genre (name) VALUES ('some genre2');
+INSERT INTO `spring_library`.genre (name) VALUES ('some genre3');
+INSERT INTO `spring_library`.genre (name) VALUES ('some genre2');
 
-INSERT INTO `spring-library`.publisher (name) VALUES ('some publisher');
+INSERT INTO `spring_library`.publisher (name) VALUES ('some publisher1');
+INSERT INTO `spring_library`.publisher (name) VALUES ('some publisher2');
+INSERT INTO `spring_library`.publisher (name) VALUES ('some publisher3');
 
-INSERT INTO `spring-library`.book (name, content, page_count, isbn, genre_id, author_id, publish_year, publisher_id, image) VALUES ('book name', ?, '123', '123-123-123-123', '1', '1', '1999', '1', ?);
+INSERT INTO `spring_library`.book (name, content, page_count, isbn, genre_id, author_id, publish_year, publisher_id, image) VALUES ('book name1', ?, '123', '123-123-123-123', '1', '1', '1999', '1', ?);
+INSERT INTO `spring_library`.book (name, content, page_count, isbn, genre_id, author_id, publish_year, publisher_id, image) VALUES ('book name2', ?, '223', '223-123-123-123', '2', '2', '1999', '2', ?);
+INSERT INTO `spring_library`.book (name, content, page_count, isbn, genre_id, author_id, publish_year, publisher_id, image) VALUES ('book name3', ?, '323', '323-123-123-123', '3', '3', '1999', '3', ?);
 
-INSERT INTO `spring-library`.book (name, content, page_count, isbn, genre_id, author_id, publish_year, publisher_id) VALUES ('book name', ?, '123', '123-123-123-123', '1', '1', '1999', '1');
+# INSERT INTO `spring_library`.book (name, content, page_count, isbn, genre_id, author_id, publish_year, publisher_id) VALUES ('book name', ?, '123', '123-123-123-123', '1', '1', '1999', '1');
